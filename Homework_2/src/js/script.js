f1d3156b15ca2417cd4da09fc7a0479e32c73085
KasperKaljuste <kasperk@tuta.io>
2022-10-30 16:26:25 +0200

    Posts in middle

--- Homework_2/src/js/script.js
+++ Homework_2/src/js/script.js
@@ -2,11 +2,8 @@ window.onload = function() {
     fetch('http://myjson.dit.upm.es/api/bins/38me')
         .then((response) => response.json())
         .then(json => {
-            let middle = document.createElement("div");
-            middle.className = "middle";
-
-            let middle_postbar = document.createElement("div");
-            middle_postbar.className = "middle_postbar";
+            let middle_postbar = document.getElementById("posts");
+            
             for (const i in json){
                 
                 let post = document.createElement("div");
@@ -14,8 +11,13 @@ window.onload = function() {
 
                 let post_head = document.createElement("div");
                 post_head.className = "post_header";
-                post_head.innerText = json[i].time;
-                
+                post_head.innerHTML = 
+                '<a><img id="profile_pic" src="res/images/me.png" width="40" height="40" alt="My picture"></a>';
+                let post_time = document.createElement("p");
+                post_time.innerHTML = json[i].time;
+                post_head.appendChild(post_time);
+
+
                 let post_body = document.createElement("div");
                 post_body.className = "post_body";
                 post_body.innerText = json[i].body;
@@ -25,10 +27,6 @@ window.onload = function() {
                 
                 post.appendChild(post_head);
                 post.appendChild(post_body);
-                middle_postbar.appendChild(post);
-                middle.appendChild(middle_postbar);
-                document.body.appendChild(middle);
-                console.log(json[i]);
-                
+                middle_postbar.appendChild(post);  
             }
         });}
