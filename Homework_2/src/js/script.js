967c66997db719faab20b937ac3d8bc75d2c06d0
uschults <u.schults@gmail.com>
2022-10-31 17:02:49 +0200

    hw2

--- Homework_2/src/js/script.js
+++ Homework_2/src/js/script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-    fetch('http://myjson.dit.upm.es/api/bins/b6vy')
+    //fetch('http://myjson.dit.upm.es/api/bins/b6vy')
+    fetch('res/json/posts.json')
         .then((response) => response.json())
         .then(json => {
             let middle_postbar = document.getElementById("posts");
@@ -11,9 +12,20 @@ window.onload = function() {
 
                 let post_head = document.createElement("div");
                 post_head.className = "post_head";
-                post_head.innerHTML = 
-                '<a><img id="profile_pic" src="res/images/me.png" width="40" height="40" alt="My picture"></a>';
+
+                //let post_profile_image = document.createElement("a");
+                //post_profile_image.textContent = <img id="profile_pic" src="res/images/me.png" width="40" height="40" alt="My picture">;
+                //post_head.appendChild(post_profile_image);
+
+                post_head.innerHTML = '<a><img id="profile_pic" src="res/images/me.png" width="40" height="40" alt="My picture"></a>';
+                
+                let post_name = document.createElement("p");
+                post_name.className = "post_name";
+                post_name.innerHTML = json[i].name;
+                post_head.appendChild(post_name);
+
                 let post_time = document.createElement("p");
+                post_time.className = "post_time"
                 post_time.innerHTML = json[i].time;
                 post_head.appendChild(post_time);
 
@@ -30,7 +42,7 @@ window.onload = function() {
 
                 let post_footer = document.createElement("div");
                 post_footer.className = "post_footer"
-                post_footer.innerHTML = '<button type="submit" class="like_button" id="like_button"><img src="res/images/like_button.png" width="40" height="40" alt="Like" /></button><p>Likes: 69</p> <p>Comments: 42</p>'
+                post_footer.innerHTML = '<button type="submit" class="like_button" id="like_button"><img src="res/images/like_button.png" width="40" height="40" alt="Like" /></button><p>Likes: '+ json[i].likes +'</p> <p>Comments:'+ json[i].comments +'</p>'
 
                 
                 post.appendChild(post_head);
@@ -38,4 +50,5 @@ window.onload = function() {
                 post.appendChild(post_footer);
                 middle_postbar.appendChild(post);  
             }
-        });}
+        });
+    }
